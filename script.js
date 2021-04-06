dd628b1303e264bcb22827adefe7c1dbc0165236
Yuliia-Sydorovych <yuliiasydorovych@gmail.com>
2021-04-06 19:36:23 +0300

    new changes

--- script.js
+++ script.js
@@ -1,31 +1,4 @@
 
-function loadClient() {
-    gapi.client.setApiKey("AIzaSyAufstWATGaD2bTiDzZ8aohlb-YB_DFQIE");
-    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-        .then(function() { console.log("GAPI client loaded for API"); },
-            function(err) { console.error("Error loading GAPI client for API", err); });
-}
-
-function execute() {
-    return gapi.client.youtube.channels.list({
-    "part": [
-        "snippet,contentDetails,statistics"
-    ],
-    "id": [
-        "UC_x5XG1OV2P6uZZ5FSM9Ttw"
-    ]
-    })
-        .then(function(response){
-            
-                console.log("Response", response);
-            },
-            function(err) { console.error("Execute error", err); });
-}
-gapi.load("client:auth2", function() {
-    gapi.auth2.init({client_id: "YOUR_CLIENT_ID"});
-});
-
-
 
 /*
 class YouTube
