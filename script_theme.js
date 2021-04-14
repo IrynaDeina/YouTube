2a3563d45741e3a0cdd4c9d82e2f15ced00aacb9
hurachima <62026617+hurachima@users.noreply.github.com>
2021-04-14 16:24:59 +0300

    create dark theme

--- script_theme.js
+++ script_theme.js
@@ -1,28 +1,38 @@
-var btn = document.getElementById("theme_button");
-var link = document.getElementById("theme_link");
+// var btn = document.getElementById("theme_button");
+// var link = document.getElementById("theme_link");
 
-btn.addEventListener("click", function () { ChangeTheme(); });
+// btn.addEventListener("click", function () { ChangeTheme(); });
 
-function ChangeTheme()
-{
-    let lightTheme = "light.css";
-    let darkTheme = "dark.css";
+// function ChangeTheme()
+// {
+//     let lightTheme = "light.css";
+//     let darkTheme = "dark.css";
 
-    var currTheme = link.getAttribute("href");
-    var theme = "";
+//     var currTheme = link.getAttribute("href");
+//     var theme = "";
 
-    if(currTheme == lightTheme)
-    {
-    currTheme = darkTheme;
-    theme = "dark";
-    }
-    else
-    {    
-    currTheme = lightTheme;
-    theme = "light";
-    }
+//     if(currTheme == lightTheme)
+//     {
+//     currTheme = darkTheme;
+//     theme = "dark";
+//     }
+//     else
+//     {    
+//     currTheme = lightTheme;
+//     theme = "light";
+//     }
 
-    link.setAttribute("href", currTheme);
-    console.log(currTheme);
+//     link.setAttribute("href", currTheme);
+//     console.log(currTheme);
+// }
+
+
+if(!localStorage.theme) localStorage.theme = 'light';
+document.body.className = localStorage.theme;
+
+let switchMode = document.querySelector('#switch');
+
+switchMode.onclick = () => {
+    document.body.classList.toggle('dark');
+    localStorage.theme = document.body.className || 'light';
 }
-
